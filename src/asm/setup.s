f7225dafeae5231df77386446beadd54f93ba31a
lllzebralll <alexei.zebra@yandex.ru>
2023-02-08 17:40:57 +0300

    fix setup

--- src/asm/setup.s
+++ src/asm/setup.s
@@ -4,7 +4,7 @@
 use16
 org 0500h
 
-; code
+.code:
 
 xor ax,ax
 push ax
@@ -26,39 +26,49 @@ mov si,paket_sysint
 mov ah,42h
 int 13h
 
-mov ax,4F02h
-mov bx,11bh
-int 10h
+; mov ax,4F02h
+; mov bx,11bh
+; int 10h
 
 aboba:
-xor ax,ax
-mov ah,14h
-int 90h
+    xor ax,ax
+    int 16h
 cmp al,3Dh
 je obabo
-mov ah,13h
-int 90h
+	push ax
+	mov ah,0fh
+    int 10h
+	pop ax
+	mov ah,0ah
+	mov cx,1
+	int 10h
 jmp aboba
 obabo:
 
+; Open gate A20
 in al, 92h
 or al, 2
 out 92h, al
 
+; Disable maskable interrupts
 cli
+
+; Disable non-maskable interrupts
 in al, 70h
 or al, 80h
 out 70h, al
 
 lgdt [gdtr]
 
+; Switch to protected mode
 mov eax, cr0
 or al, 1
 mov cr0, eax
 
 jmp far dword 0000000000001000b:pm_entry
 
-use32               ; generate 32-bit code
+use32
+
 ; Protected mode entry point
 pm_entry:
     mov ax, 0000000000010000b
@@ -82,7 +92,6 @@ pm_entry:
     jmp .loop
 
 .exit:
-    jmp $
     cli                         
     hlt                         
 
@@ -90,18 +99,18 @@ message db 'Hello World!', 0
 
 align 8
 
-push cs
-pop ds
-mov si,paket_kernel
-mov ah,42h
-int 13h
-jmp 0000:8000h
+; push cs
+; pop ds
+; mov si,paket_kernel
+; mov ah,42h
+; int 13h
+; jmp 0000:8000h
 
-jmp $
+; jmp $
 
 times(512-($-0500h)) db 0
 
-;data
+.data:
 
 gdt:
 NULL_SEG_DESCRIPTOR db 8 dup(0)
@@ -136,7 +145,7 @@ paket_kernel:
 paket_sysint:
     dw 16;const paksize
     dw 2;num sectors
-    dw 700h;offset
+    dw 900h;offset
     dw 0;segment
     dq 3;start sector
 
