a8a600bc6a670fc880fe2ca8f537ebcd3c8b1518
lllzebralll <alexei.zebra@yandex.ru>
2023-03-28 19:50:16 +0300

    tab fix

--- include/lib/rom/ide.h
+++ include/lib/rom/ide.h
@@ -149,72 +149,72 @@ ide_initialize (unsigned int BAR0, unsigned int BAR1, unsigned int BAR2,
     for (i = 0; i < 2; i++)
         for (j = 0; j < 2; j++) {
  
-         unsigned char err = 0, type = IDE_ATA, status;
-         ide_devices[count].Reserved = 0; // Assuming that no drive here.
+            unsigned char err = 0, type = IDE_ATA, status;
+            ide_devices[count].Reserved = 0; // Assuming that no drive here.
  
-         // (I) Select Drive:
-         ide_write(i, ATA_REG_HDDEVSEL, 0xA0 | (j << 4)); // Select Drive.
-         sleep(1); // Wait 1ms for drive select to work.
+            // (I) Select Drive:
+            ide_write(i, ATA_REG_HDDEVSEL, 0xA0 | (j << 4)); // Select Drive.
+            sleep(1); // Wait 1ms for drive select to work.
  
-         // (II) Send ATA Identify Command:
-         ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY);
-         sleep(1); // This function should be implemented in your OS. which waits for 1 ms.
+            // (II) Send ATA Identify Command:
+            ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY);
+            sleep(1); // This function should be implemented in your OS. which waits for 1 ms.
                    // it is based on System Timer Device Driver.
  
-         // (III) Polling:
-         if (ide_read(i, ATA_REG_STATUS) == 0) continue; // If Status = 0, No Device.
+            // (III) Polling:
+            if (ide_read(i, ATA_REG_STATUS) == 0) continue; // If Status = 0, No Device.
  
-         while(1) {
-            status = ide_read(i, ATA_REG_STATUS);
-            if ((status & ATA_SR_ERR)) {err = 1; break;} // If Err, Device is not ATA.
-            if (!(status & ATA_SR_BSY) && (status & ATA_SR_DRQ)) break; // Everything is right.
-         }
+            while(1) {
+                status = ide_read(i, ATA_REG_STATUS);
+                if ((status & ATA_SR_ERR)) {err = 1; break;} // If Err, Device is not ATA.
+                if (!(status & ATA_SR_BSY) && (status & ATA_SR_DRQ)) break; // Everything is right.
+            }
  
-         // (IV) Probe for ATAPI Devices:
+            // (IV) Probe for ATAPI Devices:
  
-         if (err != 0) {
-            unsigned char cl = ide_read(i, ATA_REG_LBA1);
-            unsigned char ch = ide_read(i, ATA_REG_LBA2);
+            if (err != 0) {
+                unsigned char cl = ide_read(i, ATA_REG_LBA1);
+                unsigned char ch = ide_read(i, ATA_REG_LBA2);
  
-            if (cl == 0x14 && ch ==0xEB)
-               type = IDE_ATAPI;
-            else if (cl == 0x69 && ch == 0x96)
-               type = IDE_ATAPI;
+                if (cl == 0x14 && ch ==0xEB)
+                type = IDE_ATAPI;
+                else if (cl == 0x69 && ch == 0x96)
+                type = IDE_ATAPI;
+                else
+                continue; // Unknown Type (may not be a device).
+    
+                ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY_PACKET);
+                sleep(1);
+            }
+ 
+            // (V) Read Identification Space of the Device:
+            ide_read_buffer(i, ATA_REG_DATA, (unsigned int *)ide_buf, 128);
+    
+            // (VI) Read Device Parameters:
+            ide_devices[count].Reserved     = 1;
+            ide_devices[count].Type         = type;
+            ide_devices[count].Channel      = i;
+            ide_devices[count].Drive        = j;
+            ide_devices[count].Signature    = *((unsigned short *)(ide_buf + ATA_IDENT_DEVICETYPE));
+            ide_devices[count].Capabilities = *((unsigned short *)(ide_buf + ATA_IDENT_CAPABILITIES));
+            ide_devices[count].CommandSets  = *((unsigned int *)(ide_buf + ATA_IDENT_COMMANDSETS));
+ 
+            // (VII) Get Size:
+            if (ide_devices[count].CommandSets & (1 << 26))
+                // Device uses 48-Bit Addressing:
+                ide_devices[count].Size   = *((unsigned int *)(ide_buf + ATA_IDENT_MAX_LBA_EXT));
             else
-               continue; // Unknown Type (may not be a device).
- 
-            ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY_PACKET);
-            sleep(1);
-         }
- 
-         // (V) Read Identification Space of the Device:
-         ide_read_buffer(i, ATA_REG_DATA, (unsigned int *)ide_buf, 128);
- 
-         // (VI) Read Device Parameters:
-         ide_devices[count].Reserved     = 1;
-         ide_devices[count].Type         = type;
-         ide_devices[count].Channel      = i;
-         ide_devices[count].Drive        = j;
-         ide_devices[count].Signature    = *((unsigned short *)(ide_buf + ATA_IDENT_DEVICETYPE));
-         ide_devices[count].Capabilities = *((unsigned short *)(ide_buf + ATA_IDENT_CAPABILITIES));
-         ide_devices[count].CommandSets  = *((unsigned int *)(ide_buf + ATA_IDENT_COMMANDSETS));
- 
-         // (VII) Get Size:
-         if (ide_devices[count].CommandSets & (1 << 26))
-            // Device uses 48-Bit Addressing:
-            ide_devices[count].Size   = *((unsigned int *)(ide_buf + ATA_IDENT_MAX_LBA_EXT));
-         else
-            // Device uses CHS or 28-bit Addressing:
-            ide_devices[count].Size   = *((unsigned int *)(ide_buf + ATA_IDENT_MAX_LBA));
- 
-         // (VIII) String indicates model of device (like Western Digital HDD and SONY DVD-RW...):
-         for(k = 0; k < 40; k += 2) {
-            ide_devices[count].Model[k] = ide_buf[ATA_IDENT_MODEL + k + 1];
-            ide_devices[count].Model[k + 1] = ide_buf[ATA_IDENT_MODEL + k];}
-         ide_devices[count].Model[40] = 0; // Terminate String.
- 
-         count++;
-      }
+                // Device uses CHS or 28-bit Addressing:
+                ide_devices[count].Size   = *((unsigned int *)(ide_buf + ATA_IDENT_MAX_LBA));
+    
+            // (VIII) String indicates model of device (like Western Digital HDD and SONY DVD-RW...):
+            for(k = 0; k < 40; k += 2) {
+                ide_devices[count].Model[k] = ide_buf[ATA_IDENT_MODEL + k + 1];
+                ide_devices[count].Model[k + 1] = ide_buf[ATA_IDENT_MODEL + k];}
+            ide_devices[count].Model[40] = 0; // Terminate String.
+    
+            count++;
+        }
  
     // 4- Print Summary:
     // for (i = 0; i < 4; i++)
