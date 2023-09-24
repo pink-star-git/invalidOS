26d6a95098aab9735974831e814f7c95bdd50a95
I am BATMAN <xxx.mailg@gmail.com>
2023-09-23 23:02:25 -0500

    small fix

--- include/lib/str.h
+++ include/lib/str.h
@@ -25,7 +25,7 @@ inline u_char8 strcpy(const char *src, char *dst) { // should be \0 ended
 }
 
 template <class T, u_char8 N>
-inline u_char8 strcmp(const T (&src)[N], T (&dst)[N]) { // only static arrays
+inline u_char8 strcmp(const T (&src)[N], const T (&dst)[N]) { // only static
     for (int i = 0; i < N; i++) {
         if (dst[i] < src[i]) {
             return -1;
@@ -34,8 +34,9 @@ inline u_char8 strcmp(const T (&src)[N], T (&dst)[N]) { // only static arrays
             return 1;
         }
         if (dst[i] == 0 && src[i] == 0)
-            return 0;
+            break;
     }
+    return 0;
 }
 
 inline u_char8 strcmp(const char *src, const char *dst) { // should be \0 ended
@@ -45,8 +46,9 @@ inline u_char8 strcmp(const char *src, const char *dst) { // should be \0 ended
         if (*dst > *src)
             return 1;
         if (*dst++ == 0 && *src++ == 0)
-            return 0;
+            break;
     }
+    return 0;
 }
 
 inline u_char8 memcpy(const char *src, char *dst, u_char8 size) {
@@ -105,13 +107,11 @@ class string {
     u_char8 operator[](u_char8 index) const { return data[index]; }
     u_char8 &operator[](u_char8 index) { return data[index]; }
 
-    bool operator==(const string &str) const{
+    bool operator==(const string &str) const {
         return (len == str.len) && !detail::strcmp(str.data, data);
     }
 
-    bool operator!=(const string &str) const{
-        return !(*this == str);
-    }
+    bool operator!=(const string &str) const { return !(*this == str); }
 
     string &operator+=(const string &str) {
         if (len + str.len > 255)
@@ -121,19 +121,19 @@ class string {
         return *this;
     }
 
-    string operator+(const string &str) const{
+    string operator+(const string &str) const {
         if (len + str.len > 255)
             throw "out string is overflow";
         string temp(*this);
-        temp+=str;
+        temp += str;
         return temp;
     }
 
-    string reverse() const{
-        string s(*this);
-        for (u_char8 i = 0; i < s.len / 2; ++i)
-            detail::swap(s.data[i], s.data[s.len - i - 1]);
-        return s;
+    string reverse() const {
+        string temp(*this);
+        for (u_char8 i = 0; i < temp.size() / 2; ++i)
+            detail::swap(temp[i], temp[temp.size() - i - 1]);
+        return temp;
     }
 
     void clear() { len = 0; }
@@ -144,4 +144,4 @@ class string {
     u_char8 len = 0; // should be size_t type
     u_char8 data[255] = {};
 };
-}; 
+}; // namespace sil
